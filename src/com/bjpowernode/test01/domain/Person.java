3f4272fa38650b9d25900e28448e37447ec0c3ff
limaliang <limaliang@1663674879qq.com>
2021-04-07 20:30:06 +0800

    初始导入

---
+++ src/com/bjpowernode/test01/domain/Person.java
@@ -0,0 +1,13 @@
+package com.bjpowernode.test01.domain;
+
+/**
+ * ClassName:Person
+ * Package:com.bjpowernode.test01.domain
+ * Description:
+ *
+ * @date:2021/4/7 20:13
+ * @author：李灏亮
+ */
+public class Person {
+    private String name;
+}
